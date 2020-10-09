6952c9defd1459cc8330081ca3ff684177b1756b
howtri <howeltri@oregonstate.edu>
2020-10-08 22:30:07 -0400

    Added support for flask_restful. Added logging and more comments

--- application/app/dbactions.py
+++ application/app/dbactions.py
@@ -3,36 +3,46 @@
 # This is certainly vulnerable to SQL injection as is, either regex to validate user in or strip of characters here
 
 import sqlite3
+import logging
 
+logging.basicConfig(filename='./app/logs/dbActions.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 def initialize_table():
+    """
+    Creates sqlite table DOMAINSTATS with columnds DOMAIN, PORT, STATUS
+    """
     conn = sqlite3.connect('cli-test')
-    print("Opened database successfully")
+    logging.info("Opened database successfully")
 
     conn.execute('''CREATE TABLE DOMAINSTATS
              (DOMAIN CHAR(50) PRIMARY KEY     NOT NULL,
              PORT           INT    NOT NULL,
              STATUS CHAR(50) NOT NULL);''')
 
-    print("Table created successfully")
+    logging.info("Table created successfully")
 
 
 def read_table():
+    """
+    Outputs all rows in DOMAINSTATS as a tuple
+    """
     conn = sqlite3.connect('cli-test')
-    print("Opened database successfully")
+    logging.info("Opened database successfully")
     cursor = conn.execute("SELECT domain, port, status from DOMAINSTATS")
-    # for row in cursor:
-    #     statuses.append((row[0], row[1], row[2]))
-
     statuses = [(row[0], row[1], row[2]) for row in cursor]
-    print("Rows Returned")
+    logging.info("Rows Returned")
     conn.close()
     return statuses
 
 
 def write_table(domain, port, status):
+    """
+    Writes new DOMAIN PORT STATUS to DOMAINSTATS. If the domain is already present then the exception will update
+    with the new status
+    """
     conn = sqlite3.connect('cli-test')
-    print("Opened database successfully")
+    logging.info("Opened database successfully")
 
     # if greater than 10 push out last and write as first
 
@@ -41,12 +51,12 @@ def write_table(domain, port, status):
               VALUES (?,?,?)", (domain, port, status))
 
     except sqlite3.IntegrityError:
-        print('Updating')
+        logging.info(f'Updating {domain} as {status} ')
         cursor = conn.cursor()
         cursor.execute("""UPDATE DOMAINSTATS SET status=? WHERE domain=?;""", (status, domain))
         cursor.close()
-        print('Update Successful!')
+        logging.info('Update Successful!')
 
     conn.commit()
-    print("Records created successfully")
+    logging.info(f'Write successful for {domain}:{port} status: {status}')
     conn.close()
--- application/app/routes.py
+++ application/app/routes.py
@@ -2,6 +2,8 @@
 # Site Connectivity Checker webapp using Flask, sqlite, and Nginx. Deployed with Ansible
 
 from flask import Flask, redirect, url_for, request, render_template
+import flask_restful
+import logging
 import socket
 import multiprocessing.pool
 import functools
@@ -9,9 +11,14 @@ from app import app
 from app import dbactions
 
 
+logging.basicConfig(filename='./app/logs/routes.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
+
 @app.route('/')
 def index():
+    """Landing page for the app, asks for a domain name and sends data via POST to /run"""
     return render_template('start.html')
+
 
 def timeout(time):
     """Timeout decorator, time is seconds until timeout error raised"""
@@ -43,13 +50,16 @@ def connect(domain, port):
     """
     try:
         tcp_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        available = tcp_connect.connect_ex((host, port))
+        available = tcp_connect.connect_ex((domain, port))
+        logging.info(f'TCP connection to {domain}:{port}. Status {available}')
         tcp_connect.close()
         return available
+
     except socket.gaierror as dnsError:
-        print(f'Domain name failed to resolve to IP. Error: {dnsError}')
+        logging.info(f'Domain name failed to resolve to IP. Error: {dnsError} Domain: {domain}:{port}')
+
     except Exception as genericError:
-        print(f'Something else went wrong. Error: {genericError}')
+        logging.error(f'Something else went wrong. Error: {genericError} Domain: {domain}:{port}')
 
 
 @app.route('/connect/<domain>')
@@ -65,24 +75,31 @@ def status(domain, port=80):
         domain = split[0]
 
         try:
-            port_in = int(split[1])
-            port = port_in
+            # ensures that the str following : is a number and is 5 or less digits
+            if len(port_in) <= 5:
+                port_in = int(split[1])
+                port = port_in
+
         except ValueError as notInt:
-            print(f'Port could not be converted to int. Error {notInt}')
+            logging.info(f'Port could not be converted to int. Error {notInt} Domain: {domain}:{port}' )
 
     # sanitizes the domain by removing any / or ;
     sanitized = sanitize(domain)
+    logging.info(f'sanitized Domain: {domain}:{port}')
 
     try:
+        # initializes sqlite DB table DOMAINSTATS
         dbactions.initialize_table()
     except:
-        print('Table already initialized')
+        logging.info('Table already initialized')
 
     # connect method returns 0 when the connection was successful
     if connect(sanitized, port) == 0:
+        # connection succeeded
         connected = f'{sanitized}:{port} is up \n'
         dbactions.write_table(sanitized, port, 'OK')
     else:
+        # connection failed
         connected = f'{sanitized}:{port} seems to be down \n'
         dbactions.write_table(sanitized, port, 'DOWN')
 
@@ -97,9 +114,11 @@ def run():
     """
     if request.method == 'POST':
         domain = request.form['domname']
+        logging.info(f'POST Request for {domain}')
         return redirect(url_for('status', domain=domain))
     else:
         domain = request.args.get('domname')
+        logging.info(f'GET Request for {domain}')
         return redirect(url_for('status', domain=domain))
 
 
@@ -112,3 +131,8 @@ def sanitize(domain):
     if '/' in domain:
         domain = domain[:domain.index('/')]
     return domain
+
+# def export_json():
+#     """
+#     Outputs all connection information as JSON for GET request
+#     """
